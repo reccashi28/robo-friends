f86199caa717f37cccfc4e73ca41b8e5405b25ef
Rebecca Meyer <faustorebeccabsit@gmail.com>
2020-12-27 19:28:34 +0100

    added tachyons extension

--- src/Card.js
+++ src/Card.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import 'tachyons';
 
 const Card = () => {
     return (
         <div>
             <img src="https://robohash.org/test?100x100" alt="robots"/>
             <div>
-                <h2>Bec Mey</h2>
+                <h2 className="tc">Bec Mey</h2>
                 <p>becmey@gmail.com</p>
             </div>
         </div>
