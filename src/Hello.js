63ceae8c779febffc0192a12fbe0ed843f3c284f
Rebecca Meyer <faustorebeccabsit@gmail.com>
2020-12-27 18:37:54 +0100

    created first component, added tachyons extensiom

---
+++ src/Hello.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './hello.css';
+
+class Hello extends Component{
+    render() {
+        return (
+            <div>
+                <h1 className="tc f1">Hello World!</h1>
+                <p>Welcome to React</p>
+            </div>
+        )
+    }
+
+}
+
+export default Hello;
