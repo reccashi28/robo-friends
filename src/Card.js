d44336ad5f2e43bf347c46cd52abbdab9bb42820
Rebecca Meyer <faustorebeccabsit@gmail.com>
2020-12-27 19:25:02 +0100

    removed unnecessary files. added first card component

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Card = () => {
+    return (
+        <div>
+            <img src="https://robohash.org/test?100x100" alt="robots"/>
+            <div>
+                <h2>Bec Mey</h2>
+                <p>becmey@gmail.com</p>
+            </div>
+        </div>
+    )
+}
+
+export default Card;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
-import Hello from './Hello';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
+import Card from './Card.js';
+import ReactDOM from 'react-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Hello />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+ReactDOM.render( <Card />,document.getElementById('root'));
 reportWebVitals();
