f41205d961f8b6116d5e4511ed5593a8f410d59e
Rebecca Meyer <faustorebeccabsit@gmail.com>
2020-12-28 17:06:07 +0100

    iterating through the array of objects

--- src/robots.js
+++ src/robots.js
@@ -23,4 +23,40 @@ export const robots = [
         username: 'dylanm',
         email: 'dylanm@gmail.com'
     },
+    {
+        id: 5,
+        name: 'Kevin Strong',
+        username: 'kevinstr',
+        email: 'kevinstr@gmail.com'
+    },
+    {
+        id: 6,
+        name: 'Heide Galven',
+        username: 'haidgalv',
+        email: 'haidgalv@gmail.com'
+    },
+    {
+        id: 7,
+        name: 'Kelan Spring',
+        username: 'kelanspring',
+        email: 'kelanspring@gmail.com'
+    },
+    {
+        id: 8,
+        name: 'Williams Abbott',
+        username: 'willabott',
+        email: 'willabott@gmail.com'
+    },
+    {
+        id: 9,
+        name: 'Jacob Bricks',
+        username: 'jbricks',
+        email: 'jbricks@gmail.com'
+    },
+    {
+        id: 10,
+        name: 'Joseph Mindins',
+        username: 'mindinsj',
+        email: 'mindinsj@gmail.com'
+    }
 ];
