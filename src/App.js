a4973e9bfd880c3144df9908c46bc2310f72e5ef
Rebecca Meyer <faustorebeccabsit@gmail.com>
2020-12-29 19:05:08 +0100

    using fetch api for user

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component} from 'react';
 import CardList from './CardList';
-import { robots } from './robots';
 import SearchBox from './SearchBox';
 import './App.css';
 
@@ -8,9 +7,20 @@ class App extends Component{
     constructor(){
         super()
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: ''
         }
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then( response => {
+               return response.json();
+            })
+            .then( users => {
+                this.setState({ robots: users})
+            })
+        
     }
         onSearchChange = event => {
             this.setState({ searchfield: event.target.value })
